ccb4539ebea4524ffea20c5ddb57a87ab955c70e
Yugen <yugen.tanaka@icloud.com>
2025-02-01 09:03:53 +0900

    インドネシア語、マレー語、ベトナム語を追加しました。

--- src/const/Languages.js
+++ src/const/Languages.js
@@ -1,7 +1,10 @@
-//RFC 4646に準拠
+// RFC 5646に準拠
 export const languages = {
-    ENGLISH: { id: 'en', value: '英語'},
-    CHINESE_SIN: { id: 'zh-Hans', value: '中国語(簡体字)'},
-    CHINESE_TRA: { id: 'zh-Hant', value: '中国語(繁体字)'},
-    KOREAN: { id: 'ko-KR', value: '韓国語'},
+    ENGLISH: { id: 'en', value: '英語' },
+    CHINESE_SIN: { id: 'zh-Hans', value: '中国語(簡体字)' },
+    CHINESE_TRA: { id: 'zh-Hant', value: '中国語(繁体字)' },
+    KOREAN: { id: 'ko-KR', value: '韓国語' },
+    INDONESIAN: { id: 'id', value: 'インドネシア語' },
+    MALAY: { id: 'ms', value: 'マレー語' },
+    VIETNAMESE: { id: 'vi', value: 'ベトナム語' },
 };
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/lyrics-translation-form-demo-250113/',
   plugins: [
     vue(),
     vueDevTools(),
