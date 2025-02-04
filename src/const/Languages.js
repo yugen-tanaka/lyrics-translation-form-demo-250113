814fa9be6cec6b1a5d16fe7d6a027f3cc20eab7c
Yugen <yugen.tanaka@icloud.com>
2025-02-04 20:33:17 +0900

    スペイン語を追加しました

--- src/const/Languages.js
+++ src/const/Languages.js
@@ -1,10 +1,11 @@
-// RFC 5646に準拠
+// RFC 5646に準拠 https://gist.github.com/msikma/8912e62ed866778ff8cd
 export const languages = {
     ENGLISH: { id: 'en', value: '英語' },
     CHINESE_SIN: { id: 'zh-Hans', value: '中国語(簡体字)' },
     CHINESE_TRA: { id: 'zh-Hant', value: '中国語(繁体字)' },
-    KOREAN: { id: 'ko-KR', value: '韓国語' },
+    KOREAN: { id: 'ko', value: '韓国語' },
     INDONESIAN: { id: 'id', value: 'インドネシア語' },
     MALAY: { id: 'ms', value: 'マレー語' },
     VIETNAMESE: { id: 'vi', value: 'ベトナム語' },
+    SPANISH: { id: 'es', value: 'スペイン語'},
 };
